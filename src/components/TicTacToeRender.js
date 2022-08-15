ddba152dd950f8effcb2569024384aee4cb44e6b
Javad Abbasi <eros.javad@gmail.com>
2022-08-15 13:19:32 +0430

    game completed

--- src/components/TicTacToeRender.js
+++ src/components/TicTacToeRender.js
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
 
+import { checkIfWon, cpuMoveIndex, cpuMover } from "../functions"
+
 import X from '../asset/X.png'
 import O from '../asset/O.png'
 import empty from '../asset/empty.png'
 
 const TicTacToeRender = (props) => {
   const {table, setTable} = props
+
   const [isCpuTurn, setIsCpuTurn] = useState(false) 
+  const [winner, setWinner] = useState(null)
 
   const clickHandler = (row, col) => {
-    if (table[row][col] === "" && !isCpuTurn) {
+    if (table[row][col] === "" && !isCpuTurn && winner === null) {
       setIsCpuTurn(true)
       let newTable = [...table];
       newTable[row][col] = "X";
       setTable(newTable);
-      setIsCpuTurn(false)
+      setWinner(checkIfWon(newTable));
+      setTimeout(() => {
+        if(winner === null) {
+          cpuMover(cpuMoveIndex(table), table, setTable, setWinner)
+          setIsCpuTurn(false);
+        }
+      }, 1000)
     }
   }
   const srcF = (row, col) => {
@@ -25,20 +35,21 @@ const TicTacToeRender = (props) => {
 
   return (
     <div>
+      {winner && <h1>{winner}</h1>}
       <div>
-        <img style={style} src={srcF(0,0)} alt={table[0][0]} onClick={() => clickHandler(0,0)} />
-        <img style={style} src={srcF(0,1)} alt={table[0][1]} onClick={() => clickHandler(0,1)} />
-        <img style={style} src={srcF(0,2)} alt={table[0][2]} onClick={() => clickHandler(0,2)} />
+        <img style={style} src={srcF(0,0)} alt={table[0][0]} onClick={() => clickHandler(0,0)} draggable="false" />
+        <img style={style} src={srcF(0,1)} alt={table[0][1]} onClick={() => clickHandler(0,1)} draggable="false" />
+        <img style={style} src={srcF(0,2)} alt={table[0][2]} onClick={() => clickHandler(0,2)} draggable="false" />
       </div>
       <div>
-        <img style={style} src={srcF(1,0)} alt={table[1][0]} onClick={() => clickHandler(1,0)} />
-        <img style={style} src={srcF(1,1)} alt={table[1][1]} onClick={() => clickHandler(1,1)} />
-        <img style={style} src={srcF(1,2)} alt={table[1][2]} onClick={() => clickHandler(1,2)} />
+        <img style={style} src={srcF(1,0)} alt={table[1][0]} onClick={() => clickHandler(1,0)} draggable="false" />
+        <img style={style} src={srcF(1,1)} alt={table[1][1]} onClick={() => clickHandler(1,1)} draggable="false" />
+        <img style={style} src={srcF(1,2)} alt={table[1][2]} onClick={() => clickHandler(1,2)} draggable="false" />
       </div>
       <div>
-        <img style={style} src={srcF(2,0)} alt={table[2][0]} onClick={() => clickHandler(2,0)} />
-        <img style={style} src={srcF(2,1)} alt={table[2][1]} onClick={() => clickHandler(2,1)} />
-        <img style={style} src={srcF(2,2)} alt={table[2][2]} onClick={() => clickHandler(2,2)} />
+        <img style={style} src={srcF(2,0)} alt={table[2][0]} onClick={() => clickHandler(2,0)} draggable="false" />
+        <img style={style} src={srcF(2,1)} alt={table[2][1]} onClick={() => clickHandler(2,1)} draggable="false" />
+        <img style={style} src={srcF(2,2)} alt={table[2][2]} onClick={() => clickHandler(2,2)} draggable="false" />
       </div>
     </div>
   );
--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,62 @@
 const checkIfWon = (table) => {
-  
+  const diagonal1 = [table[0][0], table[1][1], table[2][2]]
+  const diagonal2 = [table[0][2], table[1][1], table[2][0]]
+  const cols = [[table[0][0], table[1][0], table[2][0]], [table[0][1], table[1][1], table[2][1]], [table[0][2], table[1][2], table[2][2]]]
+  const rows = [[table[0][0], table[0][1], table[0][2]], [table[1][0], table[1][1], table[1][2]], [table[2][0], table[2][1], table[2][2]]]
+
+  let winner = null
+
+  // diagonal winner? or draw
+  if (diagonal1.every((cell) => cell==="X" || diagonal2.every((cell) => cell==="X"))){
+    winner = "Player Won!"
+  } else if (diagonal1.every((cell) => cell==="O" || diagonal2.every((cell) => cell==="O"))){
+    winner = "CPU Won!"
+  } else if (table.flat().every((cell) => cell!=="")) {
+    winner = "Draw!"
+  } else {
+    winner = null
+  }
+
+  // row winner?
+  for (let i = 0; i < rows.length; i++) {
+    if (rows[i].every((cell) => cell==="X")) {
+      winner = "Player Won!"
+    } else if (rows[i].every((cell) => cell==="O")) {
+      winner = "CPU Won!"
+    }
+  }
+  for (let i = 0; i < cols.length; i++) {
+    if (cols[i].every((cell) => cell==="X")) {
+      winner = "Player Won!"
+    } else if (cols[i].every((cell) => cell==="O")) {
+      winner = "CPU Won!"
+    }
+  }
+
+  return winner
 }
+const cpuMoveIndex = (table) => {
+  const emptyIndexes = [];
+  table.forEach((row, rowIndex) => {
+    row.forEach((cell, colIndex) => {
+      if (cell === "") {
+        emptyIndexes.push({ rowIndex, colIndex });
+      }
+    });
+  });
+  console.log(emptyIndexes)
+  const randomIndex = Math.floor(Math.random() * emptyIndexes.length)
+  return [emptyIndexes[randomIndex].rowIndex, emptyIndexes[randomIndex].colIndex]
+}
+
+const cpuMover = (index, table, setTable, setWinner) => {
+  if(!checkIfWon(table)) {
+    const newTable = [...table];
+    newTable[index[0]][index[1]] = "O";
+    setTable(newTable);
+    setWinner(checkIfWon(newTable));
+  }
+}
+
+
+export { checkIfWon, cpuMoveIndex, cpuMover };
